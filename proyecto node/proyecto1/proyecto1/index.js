1d847256f43c38e80646f712c6b0b70c7607a8f4
DianaJimenez09 <djimenezneculman@gmail.com>
2022-07-27 15:01:03 +0200

    git

---
+++ proyecto node/proyecto1/proyecto1/index.js
@@ -0,0 +1,30 @@
+function suma(num1,num2){
+    let resultado = num1 + num2;
+    return resultado;
+};
+
+function resta(num1,num2){
+    let resultado = num1 - num2;
+    return resultado;
+};
+
+function producto(num1,num2){
+    let resultado = num1 * num2;
+    return resultado;
+};
+
+function division(num1,num2){
+    let resultado = num1 / num2;
+    return resultado;
+};
+//////////////
+///PARTE NUEVA
+//////////////
+
+let sum = 0
+function cuadrado(n1){
+    let resultado = n1 * n1
+
+    return resultado
+}
+module.exports = {suma,resta,producto,division,cuadrado}
---
+++ proyecto node/proyecto1/proyecto1/run.js
@@ -0,0 +1,8 @@
+let myLib = require("./index");
+
+console.log(myLib.suma(2,3));
+console.log(myLib.resta(5,2));
+console.log(myLib.producto(7,9));
+console.log(myLib.division(64,4));
+
+console.log(myLib.cuadrado(2));
